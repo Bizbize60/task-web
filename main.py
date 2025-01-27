28b37bbed684e7ef55487683546cc4a93d061520
Bizbize60 <600tuna@gmail.com>
2025-01-27 03:07:12 +0300

    backend

---
+++ main.py
@@ -0,0 +1,19 @@
+from sanic import Sanic
+from sanic.response import json, text
+
+app = Sanic("WebTask")
+
+@app.post("/submit")
+async def get_answers_and_print(request):
+    username = request.form.get("username")
+    question1 = request.form.get('question1')
+    question2 = request.form.get('question2')
+    question3 = request.form.get('question3')
+    print(username,question1,question2,question3)
+    return json({
+        "status": "success",
+        "data": "deneme"
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
